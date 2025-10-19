b4d562fed6464c0dd879d67de5e3117faae0ae7d
unknown <azril3121@gmail.com>
2025-10-19 19:52:31 +0700

    mengganti deskripsi

--- src/app/aksesoris/detail_ortuszealball/page.tsx
+++ src/app/aksesoris/detail_ortuszealball/page.tsx
@@ -18,8 +18,7 @@ export default function Detail(){
     
                 <h1 className="absolute top-30 left-165 text-4xl font-bold">ORTUSEIGHT-ZEAL FS BALL</h1>
                 <h2 className="absolute top-40 left-165 text-2xl mt-3 mb-3">Putih</h2>
-                <p className="absolute top-60 left-165 text-lg text-justify w-[700] bg-[#FAFAFA] opacity-75 p-7 rounded-xl inset-shadow-sm">Tampil klasik di lapangan dengan jersey retro legendaris AC Milan. 
-                Desain ikonik Rossoneri dengan sponsor Opel yang akan membawa Anda kembali ke era keemasan sepak bola Italia.</p>
+                <p className="absolute top-60 left-165 text-lg text-justify w-[700] bg-[#FAFAFA] opacity-75 p-7 rounded-xl inset-shadow-sm">Hadirkan kontrol presisi di lapangan dengan bola futsal Ortuseight Zeal. Konstruksi panel yang kuat dan material berkualitas tinggi menjamin sentuhan akurat yang ideal untuk permainan tempo cepat.</p>
                 <h2 className="absolute top-135 left-165 text-3xl">RP. 299,000.00</h2>
                 <a href="https://wa.me/62895397432051"><button className="absolute top-145 left-165 text-2xl bg-[#204B57] text-white p-3 rounded-lg mt-3 hover:bg-black-200 cursor-pointer">BELI</button></a>
                 <div className="bg-[#FAFAFA] m-8 p-5 shadow-lg rounded-xl w-[600] h-[600] flex">
--- src/app/detail_ortuslyberte/page.tsx
+++ src/app/detail_ortuslyberte/page.tsx
@@ -20,8 +20,8 @@ export default function Detail(){
             <h2 className="absolute top-40 left-165 text-2xl mt-3 mb-3">Navy</h2>
             <h2 className="absolute top-50 left-165 text-2xl mt-3 mb-3">UKURAN</h2>
             <p className="absolute top-60 left-165 text-xl">39-40-41-42-43-44-45</p>
-            <p className="absolute top-70 left-165 text-lg text-justify w-[700] bg-[#FAFAFA] opacity-75 p-7 rounded-xl inset-shadow-sm">Tampil klasik di lapangan dengan jersey retro legendaris AC Milan. 
-            Desain ikonik Rossoneri dengan sponsor Opel yang akan membawa Anda kembali ke era keemasan sepak bola Italia.</p>
+            <p className="absolute top-70 left-165 text-lg text-justify w-[700] bg-[#FAFAFA] opacity-75 p-7 rounded-xl inset-shadow-sm">Dirancang untuk kecepatan. Ortuseight Catalyst Liberte V4 hadir dengan upper berbahan mesh super ringan yang pas di kaki, 
+                    memberikan Anda kelincahan untuk melewati lawan di lapangan.</p>
             <h2 className="absolute top-135 left-165 text-3xl">RP. 649,000.00</h2>
             <a href="https://wa.me/62895397432051"><button className="absolute top-145 left-165 text-2xl bg-[#204B57] text-white p-3 rounded-lg mt-3 hover:bg-black-200 cursor-pointer">BELI</button></a>
             <div className="bg-[#FAFAFA] m-8 p-5 shadow-lg rounded-xl w-[600] h-[600] flex">
